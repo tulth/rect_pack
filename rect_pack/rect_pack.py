4d784260e3a8652c1773a8096cd0d7eeadf05ab6
tulthix <tulthix@gmail.com>
2017-01-13 20:47:39 -0600

    rects now can rotate, and have data cargo.  minor cleanup and renaming

--- rect_pack/rect_pack.py
+++ rect_pack/rect_pack.py
@@ -4,19 +4,29 @@ import itertools
 import numpy as np
 
 
-class RectSize(object):
-    def __init__(self, width, height):
-        super().__init__()
-        self.width = width
-        self.height = height
-
 class Rect(object):
-    def __init__(self, x, y, width, height):
+    def __init__(self, x, y, width, height, data=None, rotated=False):
         super().__init__()
         self.x = x
         self.y = y
         self.width = width
         self.height = height
+        self.data = data
+        self.rotated = rotated
+
+    def copy(self, ):
+        return Rect(self.x, self.y, self.width, self.height, self.data, self.rotated)
+
+    def rotate(self, ):
+        self.width = self.height
+        self.height = self.width
+        self.rotated = not self.rotated
+
+    def __str__(self, ):
+        return "width:{} height:{} x:{} y:{} rot:{} dat:{}".format(
+            self.width, self.height,
+            self.x, self.y, self.rotated, 
+            self.data, )
 
 def disjoint(rectA, rectB):
     if ((rectA.x + rectA.width <= rectB.x) or
@@ -27,6 +37,7 @@ def disjoint(rectA, rectB):
     else:
         return False
 
+    
 def isContainedIn(rectA, rectB):
     return ((rectA.x >= rectB.x) and
             (rectA.y >= rectB.y) and
@@ -34,54 +45,39 @@ def isContainedIn(rectA, rectB):
             (rectA.y + rectA.height <= rectB.y + rectB.height))
 
 
-# class DisjointRectCollection(object):
-#     def __init__(self, width, height)
-#         super().__init__()
-#         self.rects = []
-
-#     def add(self, rect):
-#         if rect.width == 0 or rect.height == 0:
-#             return True
-#         if not self.disjoint(rect):
-#             return False
-#         self.rects.append(rect)
-#         return True
-
-#     def clear(self, ):
-#         self.rects = []
-
-#     def disjoint(self, argRect):
-#         if r.width == 0 or r.height == 0:
-#             return True
-#         for rect in self:
-#             if not self.disjoint(rect, argRect):
-#                 return False
-#         return True
-
-
-class MaxRectsBinPack(object):
+class RectPack(object):
 
     def __init__(self, width, height):
         super().__init__()
         self.binWidth = width
         self.binHeight = height
-        self.usedRectangles = []
-        self.freeRectangles = [Rect(x=0, y=0, width=width, height=height)]
+        self.packedRectList = []
+        self.freeRectList = [Rect(x=0, y=0, width=width, height=height)]
+        self.occupancyHistory = []
 
-    def insert(self, width, height):
-        newNode = self.findPositionForNewNodeBestShortSideFit(width, height)
+    def pack(self, rectList, reorder=True):
+        if reorder:
+            workingRectList = list(sorted(rectList, key= lambda rect: -rect.width * rect.height))
+        else:
+            workingRectList = rectList    
+        for rect in workingRectList:
+            self.insert(rect)
+            self.occupancyHistory.append(self.occupancy())
+    
+    def insert(self, rect):
+        newNode = self.findPositionForNewNodeBestShortSideFit(rect)
         if newNode.height == 0:
             return newNode
-        numRectanglesToProcess = len(self.freeRectangles)
+        numRectanglesToProcess = len(self.freeRectList)
         idx = 0
         while idx < numRectanglesToProcess:
-            if (self.splitFreeNode(self.freeRectangles[idx], newNode)):
-                del self.freeRectangles[idx]
+            if (self.splitFreeNode(self.freeRectList[idx], newNode)):
+                del self.freeRectList[idx]
                 idx -= 1
                 numRectanglesToProcess -= 1
             idx += 1
         self.pruneFreeList()
-        self.usedRectangles.append(newNode)
+        self.packedRectList.append(newNode)
         return newNode
 
     def isBetterFit(self, bestShortSideFit, bestLongSideFit, shortSideFit, longSideFit):
@@ -96,11 +92,13 @@ class MaxRectsBinPack(object):
                 return True
         return False
 
-    def findPositionForNewNodeBestShortSideFit(self, width, height):
-        bestNode = Rect(0, 0, 0, 0)
+    def findPositionForNewNodeBestShortSideFit(self, rect):
+        width, height = rect.width, rect.height
+        bestNode = Rect(0, 0, 0, 0) # rect.copy()
+        bestNode = rect.copy()
         bestShortSideFit = None
         bestLongSideFit = None
-        for freeRect in self.freeRectangles:
+        for freeRect in self.freeRectList:
             # Try to place the rectangle in upright (non-flipped) orientation.
             if (freeRect.width >= width and freeRect.height >= height):
                 leftoverHoriz = abs(freeRect.width - width)
@@ -120,25 +118,29 @@ class MaxRectsBinPack(object):
                 flippedShortSideFit = min(flippedLeftoverHoriz, flippedLeftoverVert)
                 flippedLongSideFit = max(flippedLeftoverHoriz, flippedLeftoverVert)
                 if self.isBetterFit(bestShortSideFit, bestLongSideFit, flippedShortSideFit, flippedLongSideFit):
+                    bestNode.width = width
+                    bestNode.height = height
+                    bestNode.rotate()
+                    # bestNode.width = height
+                    # bestNode.height = width
+                    #print(bestNode)
                     bestNode.x = freeRect.x
                     bestNode.y = freeRect.y
-                    bestNode.width = height
-                    bestNode.height = width
                     bestShortSideFit = flippedShortSideFit
                     bestLongSideFit = flippedLongSideFit
         return bestNode
 
     def pruneFreeList(self):
         i = 0
-        while (i < len(self.freeRectangles)):
+        while (i < len(self.freeRectList)):
             j = i + 1
-            while (j < len(self.freeRectangles)):
-                if isContainedIn(self.freeRectangles[i], self.freeRectangles[j], ):
-                    del self.freeRectangles[i]
+            while (j < len(self.freeRectList)):
+                if isContainedIn(self.freeRectList[i], self.freeRectList[j], ):
+                    del self.freeRectList[i]
                     i -= 1
                     break
-                if isContainedIn(self.freeRectangles[j], self.freeRectangles[i], ):
-                    del self.freeRectangles[j]
+                if isContainedIn(self.freeRectList[j], self.freeRectList[i], ):
+                    del self.freeRectList[j]
                     j -= 1
                 j += 1
             i += 1
@@ -153,31 +155,31 @@ class MaxRectsBinPack(object):
             if (usedNode.y > freeNode.y and usedNode.y < freeNode.y + freeNode.height):
                 newNode = Rect(freeNode.x, freeNode.y, freeNode.width, freeNode.height, )
                 newNode.height = usedNode.y - newNode.y
-                self.freeRectangles.append(newNode)
+                self.freeRectList.append(newNode)
             #  New node at the bottom side of the used node.
             if (usedNode.y + usedNode.height < freeNode.y + freeNode.height):
                 newNode = Rect(freeNode.x, freeNode.y, freeNode.width, freeNode.height, )
                 newNode.y = usedNode.y + usedNode.height
                 newNode.height = freeNode.y + freeNode.height - (usedNode.y + usedNode.height)
-                self.freeRectangles.append(newNode)
+                self.freeRectList.append(newNode)
         if (usedNode.y < freeNode.y + freeNode.height and usedNode.y + usedNode.height > freeNode.y):
             #  New node at the left side of the used node.
             if (usedNode.x > freeNode.x and usedNode.x < freeNode.x + freeNode.width):
                 newNode = Rect(freeNode.x, freeNode.y, freeNode.width, freeNode.height, )
                 newNode.width = usedNode.x - newNode.x
-                self.freeRectangles.append(newNode)
+                self.freeRectList.append(newNode)
             #  New node at the right side of the used node.
             if (usedNode.x + usedNode.width < freeNode.x + freeNode.width):
                 newNode = Rect(freeNode.x, freeNode.y, freeNode.width, freeNode.height, )
                 newNode.x = usedNode.x + usedNode.width
                 newNode.width = freeNode.x + freeNode.width - (usedNode.x + usedNode.width)
-                self.freeRectangles.append(newNode)
+                self.freeRectList.append(newNode)
         return True;
 
 
     def occupancy(self):
         usedSurfaceArea = 0
-        for usedRect in self.usedRectangles:
+        for usedRect in self.packedRectList:
             usedSurfaceArea += usedRect.width * usedRect.height
         return usedSurfaceArea / (self.binWidth * self.binHeight)
 
@@ -186,38 +188,29 @@ def pairwiseIter(iterable):
     a = iter(iterable)
     return zip(a, a)
 
-
 def main(argv):
     if ((len(argv) < 5) or (len(argv) % 2 != 1)):
-        print("Usage: ./rect_pack.py binWidth binHeight w_0 h_0 w_1 h_1 w_2 h_2 ... w_n h_n\n")
+        print("Usage: RectPackTest binWidth binHeight w_0 h_0 w_1 h_1 w_2 h_2 ... w_n h_n\n")
         print("       where binWidth and binHeight define the size of the bin.\n")
         print("       w_i is the width of the i'th rectangle to pack, and h_i the height.\n")
-        print("Example: ./rect_pack.py 256 256 30 20 50 20 10 80 90 20\n")
+        print("Example: RectPackTest 256 256 30 20 50 20 10 80 90 20\n")
     argNums = [float(arg) for arg in argv[1:]]
     binSize = argNums[0:2]
     print("Initializing bin to size {}x{}".format(*binSize))
-    rectBin = MaxRectsBinPack(*binSize)
+    rectBin = RectPack(*binSize)
     widthByHeights = [(rectWidth, rectHeight,) for rectWidth, rectHeight in pairwiseIter(argNums[2:])]
-    # widthByHeights = []
-    # np.random.seed(42)
-    # for i in range(200):
-    #     width = np.random.uniform(5, 20)
-    #     height = np.random.uniform(5, 20)
-    #     widthByHeights.append((width, height))
-    widthByHeights = list(sorted(widthByHeights, key= lambda wByH: -wByH[0] * wByH[1]))  # decreasing area
-    #widthByHeights = list(reversed(sorted(widthByHeights, key= lambda wByH: (min(*wByH), max(*wByH)))))  # short side, then long
-    # widthByHeights = list(reversed(sorted(widthByHeights, key= lambda wByH: wByH[0] + wByH[1])))  # decreasing perimiter
-    # widthByHeights = list(reversed(sorted(widthByHeights, key= lambda wByH: max(wByH[0], wByH[1])))) # decreasing max side length
+    # widthByHeights = list(sorted(widthByHeights, key= lambda wByH: -wByH[0] * wByH[1]))
+    inRects = [Rect(0, 0, width, height) for width, height in widthByHeights]
+    rectBin.pack(inRects)
     failedCount = 0
-    for num, (rectWidth, rectHeight) in enumerate(widthByHeights):
+    for num, (packedRect, occupancy) in enumerate(zip(rectBin.packedRectList, rectBin.occupancyHistory)):
+        rectWidth, rectHeight = packedRect.width, packedRect.height
         print("# {} Packing rectangle of size {}x{}: ".format(num, rectWidth, rectHeight), end="")
-        # Perform the packing.
-        packedRect = rectBin.insert(rectWidth, rectHeight)
         # Test success or failure.
         if packedRect.height > 0:
             print("Packed to (x,y)=({},{}), (w,h)=({},{}). Free space left: {:.2f}%".format(
                 packedRect.x, packedRect.y, packedRect.width, packedRect.height,
-                100.0 - rectBin.occupancy()*100.0))
+                100.0 - occupancy*100.0))
         else:
             print("Failed! Could not find a proper position to pack this rectangle into. Skipping this one.")
             failedCount += 1
@@ -225,6 +218,23 @@ def main(argv):
         print("Done. All rectangles packed.")
     else:
         print("Done. {} rectangles failed packing.".format(failedCount))
+        
+    # for num, (rectWidth, rectHeight) in enumerate(widthByHeights):
+    #     print("# {} Packing rectangle of size {}x{}: ".format(num, rectWidth, rectHeight), end="")
+    #     # Perform the packing.
+    #     packedRect = rectBin.insert(Rect(0, 0, rectWidth, rectHeight))
+    #     # Test success or failure.
+    #     if packedRect.height > 0:
+    #         print("Packed to (x,y)=({},{}), (w,h)=({},{}). Free space left: {:.2f}%".format(
+    #             packedRect.x, packedRect.y, packedRect.width, packedRect.height,
+    #             100.0 - rectBin.occupancy()*100.0))
+    #     else:
+    #         print("Failed! Could not find a proper position to pack this rectangle into. Skipping this one.")
+    #         failedCount += 1
+    # if not failedCount:
+    #     print("Done. All rectangles packed.")
+    # else:
+    #     print("Done. {} rectangles failed packing.".format(failedCount))
     
 
     ## let's plot it
@@ -232,10 +242,10 @@ def main(argv):
     import matplotlib.colorbar as cbar
     colors = pl.cm.jet(np.asarray(range(len(widthByHeights))) / len(widthByHeights))
     ax = pl.subplot(111)
-    rectBin = MaxRectsBinPack(*binSize)
+    rectBin = RectPack(*binSize)
     for color, (rectWidth, rectHeight) in zip(colors, widthByHeights):
         # Perform the packing.
-        packedRect = rectBin.insert(rectWidth, rectHeight)
+        packedRect = rectBin.insert(Rect(0, 0, rectWidth, rectHeight))
         # Test success or failure.
         if packedRect.height > 0:
             import random
